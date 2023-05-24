a1b9fff67fe7a8d22de6107188ca143da68de68f
Nina Emiko <nina.emiko.mcnair@gmail.com>
2023-05-24 18:08:11 -0400

    Initial commit

---
+++ demo/src/main/java/com/DuSchedule/demo/DuScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.DuSchedule.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuScheduleApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/DuSchedule/demo/DuScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DuSchedule.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
